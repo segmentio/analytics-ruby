9ea1605dcfdbc3ce1dd9b40b5762a0e92b12cb39
Stephane Bellity <sbellity@gmail.com>
2013-03-12 20:20:16 +0100

    Ignore calls to Analytics.track & Analytics.identify if the client is not initialized

--- lib/analytics-ruby.rb
+++ lib/analytics-ruby.rb
@@ -1,17 +1,23 @@
-
-require 'forwardable'
 require 'analytics-ruby/version'
 require 'analytics-ruby/client'
 
 module AnalyticsRuby
-  extend SingleForwardable
-
-  def_delegators :@client, :track, :identify
 
   # By default use a single client for the module
   def self.init(options = {})
     @client = AnalyticsRuby::Client.new(options)
   end
+
+  def self.track options
+    return false unless @client
+    @client.track(options)
+  end
+
+  def self.identify options
+    return false unless @client
+    @client.identify(options)
+  end
+
 
 end
 
