42db83a19279e211654dcdafc0e317749cccdc5a
Travis Jeffery <tj@travisjeffery.com>
2014-05-09 13:04:10 -0500

    Fix library to be an object (#44)

--- lib/segment/analytics/client.rb
+++ lib/segment/analytics/client.rb
@@ -291,7 +291,7 @@ module Segment
       #
       # context - Hash of call context
       def add_context(context)
-        context[:library] = 'analytics-ruby'
+        context[:library] =  { name: "analytics-ruby", version: Segment::Analytics::VERSION.to_s }
       end
 
       # private: Checks that the write_key is properly initialized
