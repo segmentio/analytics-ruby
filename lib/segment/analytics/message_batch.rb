f6e659b44be55a27d2d7ec94a8f7a61d226be425
Paul Kuruvilla <rohitpaulk@gmail.com>
2018-01-05 18:04:14 +0530

    Reject messages that exceed the maximum allowed size

--- lib/segment/analytics/message_batch.rb
+++ lib/segment/analytics/message_batch.rb
@@ -1,15 +1,25 @@
+require 'segment/analytics/logging'
+
 module Segment
   class Analytics
     # A batch of `Message`s to be sent to the API
     class MessageBatch
       extend Forwardable
+      include Segment::Analytics::Logging
 
       def initialize
         @messages = []
       end
 
+      def <<(message)
+        if message.too_big?
+          logger.error('a message exceeded the maximum allowed size')
+        else
+          @messages << message
+        end
+      end
+
       def_delegators :@messages, :to_json # TODO: Cache and reuse
-      def_delegators :@messages, :<<
       def_delegators :@messages, :clear
       def_delegators :@messages, :empty?
       def_delegators :@messages, :length
