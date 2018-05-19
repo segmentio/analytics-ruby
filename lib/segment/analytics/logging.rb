b55df2695b6f745cf126e5622a87503e67904da3
Paul Kuruvilla <rohitpaulk@gmail.com>
2018-05-19 16:09:29 +0530

    Add [analytics-ruby] prefix to all log messages

--- lib/segment/analytics/logging.rb
+++ lib/segment/analytics/logging.rb
@@ -2,16 +2,43 @@ require 'logger'
 
 module Segment
   class Analytics
+    # Wraps an existing logger and adds a prefix to all messages
+    class PrefixedLogger
+      def initialize(logger, prefix)
+        @logger = logger
+        @prefix = prefix
+      end
+
+      def debug(msg)
+        @logger.debug("#{@prefix} #{msg}")
+      end
+
+      def info(msg)
+        @logger.info("#{@prefix} #{msg}")
+      end
+
+      def warn(msg)
+        @logger.warn("#{@prefix} #{msg}")
+      end
+
+      def error(msg)
+        @logger.error("#{@prefix} #{msg}")
+      end
+    end
+
     module Logging
       class << self
         def logger
-          @logger ||= if defined?(Rails)
-                        Rails.logger
-                      else
-                        logger = Logger.new STDOUT
-                        logger.progname = 'Segment::Analytics'
-                        logger
-                      end
+          return @logger if @logger
+
+          base_logger = if defined?(Rails)
+                          Rails.logger
+                        else
+                          logger = Logger.new STDOUT
+                          logger.progname = 'Segment::Analytics'
+                          logger
+                        end
+          @logger = PrefixedLogger.new(base_logger, '[analytics-ruby]')
         end
 
         attr_writer :logger
