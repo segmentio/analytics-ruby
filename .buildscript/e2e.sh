0e62d149e9e02855613666b82992365339eec581
Paul Kuruvilla <rohitpaulk@gmail.com>
2019-05-27 21:17:15 +0530

    Upgrade tester version

--- .buildscript/e2e.sh
+++ .buildscript/e2e.sh
@@ -6,7 +6,7 @@ if [ "$RUN_E2E_TESTS" != "true" ]; then
   echo "Skipping end to end tests."
 else
   echo "Running end to end tests..."
-  wget https://github.com/segmentio/library-e2e-tester/releases/download/0.2.1/tester_linux_amd64 -O tester
+  wget https://github.com/segmentio/library-e2e-tester/releases/download/0.4.0/tester_linux_amd64 -O tester
   chmod +x tester
   ./tester -path='./bin/analytics'
   echo "End to end tests completed!"
