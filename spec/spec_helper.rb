a0f577f55f14b34a0482d6553d43ea55efde1915
Michael Grosse Huelsewiesche <mihuelsewiesche@twilio.com>
2024-05-01 14:43:02 -0700

    Adding new cov formatter

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,10 @@
 # frozen_string_literal: true
 
+require "simplecov"
+SimpleCov.start
+require 'simplecov-cobertura'
+SimpleCov.formatter = SimpleCov::Formatter::CoberturaFormatter
+
 require 'segment/analytics'
 require 'active_support/time'
 
