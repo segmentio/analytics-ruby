c1b795126dd8edfb6594b82036dc75c467b3e32f
Michael Grosse Huelsewiesche <mihuelsewiesche@twilio.com>
2024-05-01 14:18:46 -0700

    Remove deprecated codecov uploader

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,5 @@
 # frozen_string_literal: true
 
-# https://github.com/codecov/codecov-ruby#usage
-require 'simplecov'
-SimpleCov.start
-require 'codecov'
-SimpleCov.formatter = SimpleCov::Formatter::Codecov
-
 require 'segment/analytics'
 require 'active_support/time'
 
