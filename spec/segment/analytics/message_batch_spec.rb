bdf9d9d5d50e77d6a7c533e7ba3cbca6385941fb
Shane Duvall <sduvall@mark4tech.com>
2024-05-28 14:11:07 -0500

    Fix auto correct quotes and spaces

--- spec/segment/analytics/message_batch_spec.rb
+++ spec/segment/analytics/message_batch_spec.rb
@@ -1,43 +1,43 @@
 # frozen_string_literal: true
 
-require "spec_helper"
+require 'spec_helper'
 
 module Segment
   class Analytics
     describe MessageBatch do
       subject { described_class.new(100) }
 
-      describe "#<<" do
-        it "appends messages" do
+      describe '#<<' do
+        it 'appends messages' do
           expect(subject.length).to eq(0)
 
-          subject << {"a" => "b"}
+          subject << { 'a' => 'b' }
 
           expect(subject.length).to eq(1)
         end
 
-        it "rejects messages that exceed the maximum allowed size" do
+        it 'rejects messages that exceed the maximum allowed size' do
           max_bytes = Defaults::Message::MAX_BYTES
-          message = {"a" => "b" * max_bytes}
+          message = { 'a' => 'b' * max_bytes }
 
           expect(subject.length).to eq(0)
 
           expect { subject << message }.to raise_error(MessageBatch::JSONGenerationError)
-            .with_message("Message Exceeded Maximum Allowed Size")
+            .with_message('Message Exceeded Maximum Allowed Size')
         end
       end
 
-      describe "#full?" do
-        it "returns true once item count is exceeded" do
-          99.times { subject << {a: "b"} }
+      describe '#full?' do
+        it 'returns true once item count is exceeded' do
+          99.times { subject << { a: 'b' } }
           expect(subject.full?).to be(false)
 
-          subject << {a: "b"}
+          subject << { a: 'b' }
           expect(subject.full?).to be(true)
         end
 
-        it "returns true once max size is almost exceeded" do
-          message = {a: "b" * (Defaults::Message::MAX_BYTES - 10)}
+        it 'returns true once max size is almost exceeded' do
+          message = { a: "b" * (Defaults::Message::MAX_BYTES - 10) }
 
           message_size = message.to_json.bytesize
 
