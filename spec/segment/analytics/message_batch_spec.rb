b4506dd56b5e8b17527ca32b5e498864e2130d38
Shane Duvall <sduvall@mark4tech.com>
2024-05-28 10:30:00 -0500

    upd

--- spec/segment/analytics/message_batch_spec.rb
+++ spec/segment/analytics/message_batch_spec.rb
@@ -19,7 +19,7 @@ module Segment
 
           subject << message
           expect(subject.length).to eq(0)
-          expect { Message }.to raise_error("Message Exceeded Maximum Allowed Size")
+          expect { subject }.to raise_error("Message Exceeded Maximum Allowed Size")
         end
       end
 
